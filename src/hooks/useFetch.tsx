16ce1e41afef3891b190f96bbfcb0c9d588c7737
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-03 20:24:35 +0100

    fetch in

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -2,16 +2,32 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-export const useFetch = <T = unknown>(url:string) => {
+export const useFetch = <T = unknown>(url: string, mockWait = false) => {
 	const [items, setItems] = useState<T>([] as T);
+	const [isLoading, setIsLoading] = useState(true);
+	const [timesItemsChanged, setTimesItemsChanged] = useState(0);
+
+	useEffect(() => {
+		if (timesItemsChanged === 0) {
+			setTimesItemsChanged(1);
+		} else {
+			setIsLoading(false);
+		}
+	}, [items]);
 
 	useEffect(() => {
 		(async () => {
 			const response = await axios.get(url);
 			const _items = response.data;
-			setItems(_items);
+			if (mockWait) {
+				setTimeout(() => {
+					setItems(_items);
+				}, Math.floor(Math.random() * 2000) + 1000)
+			} else {
+				setItems(_items);
+			}
 		})();
 	}, []);
 
-	return { items };
-}
+	return { items, isLoading };
+};
--- src/pages/PageFetch.tsx
+++ src/pages/PageFetch.tsx
@@ -2,17 +2,23 @@ import { IJob, ISkill } from "../components/interfaces";
 import { useFetch } from "../hooks/useFetch";
 
 export const PageFetch = () => {
-	const { items: skills } = useFetch<ISkill[]>(
-		"https://edwardtanguay.vercel.app/share/skills_with_id.json"
+	const { items: skills, isLoading: isLoadingSkills } = useFetch<ISkill[]>(
+		"https://edwardtanguay.vercel.app/share/skills_with_id.json",
+		true
 	);
-	const { items: jobs } = useFetch<IJob[]>(
-		"https://edwardtanguay.vercel.app/share/jobs.json"
+	const { items: jobs, isLoading: isLoadingJobs } = useFetch<IJob[]>(
+		"https://edwardtanguay.vercel.app/share/jobs.json",
+		true
 	);
 
 	return (
 		<>
-			<p>There are {skills.length} skills.</p>
-			<p>There are {jobs.length} jobs.</p>
+			<p>There are {isLoadingSkills ? "..." : skills.length} skills.</p>
+			{isLoadingJobs ? (
+				<p>Jobs are loading...</p>
+			) : (
+				<p>There are {jobs.length} jobs.</p>
+			)}
 		</>
 	);
 };
